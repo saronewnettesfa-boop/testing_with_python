44406b587b3c20aa59803c6c7617c05fbadf3ce0
saronewnettesfa-boop <saronewnettesfa@gmail.com>
2025-10-21 22:54:06 -0700

    Implement unit conversion for miles and Celsius
    Added functionality to convert miles to kilometers and Celsius to Fahrenheit.

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,9 @@
-# your code here
+constant=float(1.60934)
+miles = input("Enter the number of miles: ")
+kilometers = float(miles) * constant
+kilometers = str(kilometers)
+print(miles + " miles = " + kilometers + " kilometers.")
+#-------------------------------------------
+celcius = input("Enter temperature in degree celsius: ")
+fahrenheit = (float(celcius) * 9/5) + 32
+print(str(fahrenheit) + " degree fahrenheit.")      
