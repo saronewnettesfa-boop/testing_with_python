a8cc5aac64d7fbd214bebe2e8d5d78e262d7be3b
saronewnettesfa-boop <saronewnettesfa@gmail.com>
2025-10-21 22:57:45 -0700

    Add password strength validation logic
    Implements password validation criteria for strength.

--- Problem_5_Password_Validator/solution.py
+++ Problem_5_Password_Validator/solution.py
@@ -1,1 +1,21 @@
-# your code here
+print(" Creat a strong password that contains at least: ")
+print("      - have 5 characters (long)")
+print("      - One uppercase letter")
+print("      - One lowercase letter")
+print("      - One digit")
+print("      - One special character")
+#-------------------------------------------------------
+password=input("Enter your password: ")
+length =  len(password)
+if not len(password) > 5:
+    print("❌ Weak: Password must be at least 5 characters long.")
+if not any(char.isupper() for char in password):
+    print("❌ Weak: The Password must Contain at least one uppercase letter.")   
+if not any(char.islower() for char in password):
+    print("❌ Weak: The Password must Contain at least one lowercase letter.")
+if not any(char.isdigit() for char in password):
+    print(" ❌ Weak: The Password must Contain at least one digit.")
+if not any(char in ' !@#$%^&*()-_=+[]{}|;:",.<>?/`~' for char in password):
+    print("❌ Weak: The Password must Contain at least one special character.")
+else:
+    print("✅ Strong password!")
