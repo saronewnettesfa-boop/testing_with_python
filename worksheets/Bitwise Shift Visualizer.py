69d0c59dfc23b4dd005864ab4de495e9c9a16adc
saronewnettesfa-boop <saronewnettesfa@gmail.com>
2025-10-22 05:15:11 -0700

    Implement bitwise shift visualizer
    Added functionality to input an integer and number of shift positions, and display the results of left and right bitwise shifts.

--- worksheets/Bitwise Shift Visualizer.py
+++ worksheets/Bitwise Shift Visualizer.py
@@ -1,7 +1,15 @@
-"""
-Input an integer and number of shift positions.
+integer_1=int(input("Enter first number: "))
+shift=int(input("Enter number of shif positions: "))
+print("original number: "+integer_1)
+print ("binary( befor shift):"+ bin(integer_1))
 
-Show results of left shift and right shift.
 
-Print the binary form before and after.
-"""
+left_shift = integer_1<<shift
+print("After left shift "+shift +" positions :")
+print("Decimal :"+left_shift+".")
+print("binary: "+bin(left_shift))
+
+right_shift=integer_1 >> shift
+print("after right shift "+shift +"positions: ")
+print("decimal: "+right_shift) 
+print("binary: "+bin(right_shift))
